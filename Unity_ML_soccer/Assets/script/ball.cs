386f35668dda60c3af3e73a03fd37f01e74ba2fa
hsiehjohnny <32057008+hsiehjohnny@users.noreply.github.com>
2020-04-17 09:07:09 +0800

    finish

---
+++ Unity_ML_soccer/Assets/script/ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    ///<summary>
+    ///足球進門
+    /// </summary>
+    /// 
+    public static bool complete;
+
+
+
+    ///<summary>
+    ///trigger
+    /// </summary>
+    /// 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "得分區")
+        {
+            complete = true;
+        }
+    }
+}
